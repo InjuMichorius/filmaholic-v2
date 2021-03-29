408170baeb3a11b6df268d3e632cafa58ebe8236
ÏnjuMichorius <Kato1234!>
2021-03-29 10:26:36 +0200

    Added Procfile

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const renderOverview = require('./public/source/routes/movieOverview.js')
 const renderDetail = require('./public/source/routes/movieDetail.js')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const compression = require('compression')
 
 //Code that minifies files
