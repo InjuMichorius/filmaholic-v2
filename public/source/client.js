3fa6d576717e5ca463faa8470287d176238867cb
partychickenking <partychickenking@gmail.com>
2021-04-23 09:49:51 +0200

    Added in the service worker

--- public/source/client.js
+++ public/source/client.js
@@ -1,9 +1,9 @@
-//Code for service worker
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', () => {
-//         navigator.serviceWorker
-//             .register('../serviceWorker.js')
-//             .then(reg => console.log('Service worker: registered'))
-//             .catch(err => console.log(`Service worker error: ${err}`))
-//     })
-// }
+// Code for service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('../serviceWorker.js')
+            .then(reg => console.log('Service worker: registered'))
+            .catch(err => console.log(`Service worker error: ${err}`))
+    })
+}
